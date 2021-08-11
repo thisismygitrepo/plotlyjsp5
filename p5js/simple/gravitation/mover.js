a42edb75a09c9c8ea3152765b018206d021d01f0
Alex <programmer@usa.com>
2021-08-11 13:53:44 +1000

    finished force chapter

--- p5js/simple/gravitation/mover.js
+++ p5js/simple/gravitation/mover.js
@@ -1,29 +1,35 @@
+// Gravitational Attraction
+// The Nature of Code
+// The Coding Train / Daniel Shiffman
+// https://youtu.be/EpgB3cNhKPM
+// https://thecodingtrain.com/learning/nature-of-code/2.5-gravitational-attraction.html
+// https://editor.p5js.org/codingtrain/sketches/MkLraatd
 
-class Mover{
-  constructor(x, y){
+class Mover {
+  constructor(x, y, m) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D().mult(random(3));
-    this.acc = p5.Vector.random2D().mult(random(3));
-    this.acc.setMag(0.01);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(5);
+    this.acc = createVector(0, 0);
+    this.mass = m;
+    this.r = sqrt(this.mass) * 2;
   }
 
-  update(){
-    let mouse = createVector(mouseX, mouseY);
-    this.acc = p5.Vector.sub(mouse, this.pos);
-    this.acc.setMag(5);
-    // background(v.mag());
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
 
+  update() {
     this.vel.add(this.acc);
-    this.vel.limit(2);
     this.pos.add(this.vel);
+    this.acc.set(0, 0);
   }
 
-  show(){
-    strokeWeight(2);
+  show() {
     stroke(255);
-    fill(100, 200, 100);
-    ellipse(mover.pos.x, mover.pos.y, 10, 10);
-
+    strokeWeight(2);
+    fill(255, 100);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
   }
-
 }
--- p5js/simple/gravitation/sketch.js
+++ p5js/simple/gravitation/sketch.js
@@ -1,17 +1,35 @@
+// Gravitational Attraction
+// The Nature of Code
+// The Coding Train / Daniel Shiffman
+// https://youtu.be/EpgB3cNhKPM
+// https://thecodingtrain.com/learning/nature-of-code/2.5-gravitational-attraction.html
+// https://editor.p5js.org/codingtrain/sketches/MkLraatd
 
-let mover;
+let movers = [];
+let attractor;
 
 function setup() {
-  createCanvas(400, 400);
-  mover = new Mover();
+  createCanvas(600, 600);
+  for (let i = 0; i < 10; i++) {
+    let x = random(width);
+    let y = random(height);
+    let m = random(50, 150);
+    movers[i] = new Mover(x, y, m);
+  }
+  attractor = new Attractor(width / 2, height / 2, 100);
+  background(0);
 }
 
 function draw() {
-  
   background(0);
-
-  // line(0, 0, v.x, v.y);
-  mover.update();
-  mover.show();
-  
+  for (let mover of movers) {
+    mover.update();
+    mover.show();
+    attractor.attract(mover);
+  }
+  if (mouseIsPressed) {
+    attractor.pos.x = mouseX;
+    attractor.pos.y = mouseY;
+  }
+  attractor.show();
 }
